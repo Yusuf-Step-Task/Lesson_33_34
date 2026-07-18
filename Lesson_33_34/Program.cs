64effa2a31352b5e85204e472965eff2c3fe1cbd
Bağırov Yusuf Vüqar <bagir_yv81@itstep.edu.az>
2026-07-19 02:04:09 +0400

    Add project files.

---
+++ Lesson_33_34/Program.cs
@@ -0,0 +1,14 @@
+﻿//Singleton Pattern — bir class-dan proqram boyunca yalnız 1 obyekt yaradılmasını təmin edir və həmin obyektə hər yerdən giriş imkanı verir.
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Singleton obj1 = Singleton.Instance;
+        Singleton obj2 = Singleton.Instance;
+
+        obj1.ShowMessage();
+
+        Console.WriteLine(Object.ReferenceEquals(obj1, obj2));
+    }
+}
---
+++ Lesson_33_34/Singleton.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Singleton
+{
+    private static Singleton instance;
+    private Singleton()
+    {
+        Console.WriteLine("Singleton obyekti yaraddi m");
+    }
+    public static Singleton Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new Singleton();
+            }
+            return instance;
+        }
+    }
+    public void ShowMessage()
+    {
+        Console.WriteLine("Singleton pattern");
+    }
+}
